ae9fceb5abc1eadce13563c2fb8217f832b5cc59
bouhvli <bouhali.hamza.2@gmail.com>
2024-03-26 12:02:37 +0000

    adding the route to the sign-up

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { Home } from './_root/pages';
 import SigninForm from './_auth/forms/SigninForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
+import SignupForm from './_auth/forms/SignupForm';
 const App = () => {
   return (
     <main className="flex h-screen">
         <Routes>
             {/*public routes */}
             <Route element={<AuthLayout />}>
-              <Route path="/sign-in" element={<SigninForm /> } />
+              <Route path='/sign-in' element={<SigninForm />} />
+              <Route path='/sign-up' element={<SignupForm />} />
             </Route>
             {/* private routes */}
             <Route element={<RootLayout />}>
