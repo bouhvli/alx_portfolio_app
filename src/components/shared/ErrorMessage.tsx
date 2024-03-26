6e88bfb30bd3a8af1102575c26550e8fae08d250
Nidhal-karmadi <125440528+Nidhal-karmadi@users.noreply.github.com>
2024-03-26 14:50:49 +0100

    error message component

---
+++ src/components/shared/ErrorMessage.tsx
@@ -0,0 +1,18 @@
+const ErrorMessage = () => {
+    return (
+      <div className='flex flex-1'>
+        <div className='home-container'>
+          <p className='body-medium text-light-1'>
+            Something went wrong please check your network and try again.
+          </p>
+        </div>
+        <div className='home-creators'>
+          <p className='body-medium text-light-1'>
+            Something went wrong please check your network and try again.
+          </p>
+        </div>
+      </div>
+    )
+  }
+  
+  export default ErrorMessage
