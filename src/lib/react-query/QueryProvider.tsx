7a7cdda49f961b37a3c8fcf26fc7746f451d54f0
bouhvli <bouhali.hamza.2@gmail.com>
2024-03-26 10:14:38 +0000

    setup appwrite with the app and creating the signup page with the input validation and also we added some components that we  may be needed in the future

---
+++ src/lib/react-query/QueryProvider.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+const queryClient = new QueryClient();
+export const QueryProvider = ({children}: {children: ReactNode}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
---
+++ src/lib/react-query/querykeys.ts
@@ -0,0 +1,12 @@
+export enum QUERY_KEYS {
+  GET_RECENT_POSTS = 'getRecentPosts',
+  GET_CURRENT_USER = 'getCurrentUser',
+  GET_USERS = 'getUsers',
+  GET_USER_BY_ID = 'getUserById',
+  GET_POSTS = 'getPosts',
+  GET_INFINITE_POSTS = 'getInfinitePosts',
+  GET_POST_BY_ID = 'getPostById',
+  GET_USER_POSTS = 'getUserPosts',
+  GET_FILE_PREVIEW = 'getFilePreview',
+  SEARCH_POSTS = 'getSearchPosts'
+}
